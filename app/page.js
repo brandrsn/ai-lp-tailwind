e3bfa841c8b7f93b8e5c582fa2e4c75e7718cda4
ba <brian@brandrsn.com>
2023-08-27 14:24:30 +0100

    Wee change.

--- app/page.js
+++ app/page.js
@@ -215,7 +215,7 @@ export default function Home() {
                   href="#"
                   className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Schedule A Demo
+                  Schedule A Demo!!
                 </a>
               </div>
             </nav>
