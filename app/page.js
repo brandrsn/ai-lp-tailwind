654b0a42cdb32732f39a6abc405f3476b1094c28
ba <brian@brandrsn.com>
2023-08-28 14:52:51 +0100

    CRM logos and content changes.

--- app/page.js
+++ app/page.js
@@ -10,41 +10,65 @@ const features = [
   {
     name: "Requires zero maintenance",
     description:
-      "Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.",
+      "Leverage existing content to power your chatbot, and reduce the time your staff spend on chatbot upkeep.",
     icon: NoSymbolIcon,
   },
   {
     name: "Always up-to-date",
     description:
-      "Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.",
+      "Automatically connect your content to your chatbot, meaning students always receive accurate responses.",
     icon: CheckCircleIcon,
   },
   {
     name: "Assists students 24/7",
     description:
-      "Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.",
+      "With the majority of queries being handled out-of-hours, start your Monday morning with an empty inbox.",
     icon: MoonIcon,
   },
   {
     name: "Cross-departmental",
     description:
-      "Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.",
+      "Students don't care about information siloes, so make every department's content accessible in one place.",
     icon: BuildingOffice2Icon,
   },
 ];
 const navigation = {
   main: [
-    { name: "About", href: "#" },
-    { name: "Blog", href: "#" },
-    { name: "Jobs", href: "#" },
-    { name: "Press", href: "#" },
-    { name: "Accessibility", href: "#" },
-    { name: "Partners", href: "#" },
+    { name: "About", href: "https://www.geckoengage.com/about/" },
+    { name: "Careers", href: "https://geckolabs.recruitee.com/" },
+    { name: "Contact", href: "https://www.geckoengage.com/about/contact/" },
+    {
+      name: "Privacy Policy",
+      href: "https://www.geckoengage.com/privacy-policy/",
+    },
+    { name: "Service Status", href: "http://status.geckoform.com/" },
   ],
   social: [
     {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/company/geckoengage/",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      name: "Twitter",
+      href: "https://twitter.com/geckoengage",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+        </svg>
+      ),
+    },
+    {
       name: "Facebook",
-      href: "#",
+      href: "https://www.facebook.com/geckoengage",
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -55,54 +79,19 @@ const navigation = {
         </svg>
       ),
     },
-    {
-      name: "Instagram",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Twitter",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-        </svg>
-      ),
-    },
-    {
-      name: "GitHub",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "YouTube",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
+    // {
+    //   name: "Instagram",
+    //   href: "#",
+    //   icon: (props) => (
+    //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+    //       <path
+    //         fillRule="evenodd"
+    //         d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+    //         clipRule="evenodd"
+    //       />
+    //     </svg>
+    //   ),
+    // },
   ],
 };
 const featuredTestimonial = {
@@ -195,24 +184,25 @@ const testimonials = [
 ];
 const faqs = [
   {
-    question: "How do you make holy water?",
+    question: "How does this demo work?",
     answer:
-      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Upon entering your email address, we'll identify your website domain and proceed to crawl your web pages. Then we mix in some Gecko secret sauce to create a ChatGPT-powered bot that's trained on your school's information, before using that data to answer your questions.",
   },
   {
-    question: "How do you make holy water?",
+    question:
+      "How is this different to using ChatGPT directly? What magic does Gecko provide?",
     answer:
-      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "When used out the box, ChatGPT is only knowledgeable about events prior to September 2021, and it is prone to 'hallucinating' (i.e. making up answers). Gecko uses the best bits of ChatGPT, while constraining its knowledgebase to your school's data and adding a layer of control around the messages that are created for students. Finally, ChatGPT isn't the best tool for every scenario, and Gecko leverages additional AI partners for certain tasks where appropriate.",
   },
   {
-    question: "How do you make holy water?",
+    question: "Does this integrate with our existing systems?",
     answer:
-      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "ChatGPT may be the new kid on the block, but Gecko's AI chatbot has long been integrated systems from with the main Higher Ed vendors, such as Microsoft, Ellucian, Salesforce and Slate.",
   },
   {
-    question: "How do you make holy water?",
+    question: "Where can I learn more about Gecko's chatbot product?",
     answer:
-      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Schedule a conversation with our team to learn more, or visit our website at geckoengage.com.",
   },
   // More questions...
 ];
@@ -274,8 +264,8 @@ export default function Home() {
                   than 60 seconds
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Try our interactive demo and build a chatbot that's unique to
-                  your school
+                  Try our interactive demo and build a chatbot that's 100%
+                  unique to your school
                 </p>
               </div>
 
@@ -302,7 +292,7 @@ export default function Home() {
             </div>
           </div>
           <div id="cta">
-            <div className="mx-auto max-w-7xl py-8 sm:py-16 sm:px-6  lg:px-8">
+            <div className="mx-auto max-w-7xl py-16 sm:pt-16 sm:pb-28 sm:px-6  lg:px-8">
               <div className="relative isolate overflow-hidden bg-geckoBlue px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
                 <p className="text-lg  mb-2 font-semibold leading-8 text-geckoGreen">
                   Intrigued about how AI could help your school?
@@ -350,6 +340,39 @@ export default function Home() {
                   </defs>
                 </svg>
               </div>
+            </div>
+          </div>
+          <div id="logos" className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 saturate-0"
+                src="https://uploads-ssl.webflow.com/646c5eda14230f767d1b11e9/649aed1f2aea2a0580aaf6cf_slate-logo-p-500.png"
+                alt="Slate"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 saturate-0"
+                src="https://uploads-ssl.webflow.com/646c5eda14230f767d1b11e9/649aed411d84631f84f74be0_microsoft-logo-p-800.webp"
+                alt="Microsoft"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1 saturate-0"
+                src="https://uploads-ssl.webflow.com/646c5eda14230f767d1b11e9/649aed1edaee2852cc1515de_salesforce-logo-p-500.png"
+                alt="Salesforce"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 saturate-0"
+                // src="https://uploads-ssl.webflow.com/646c5eda14230f767d1b11e9/649aed1e555c5fb2237f2107_ellucian-logo.png"
+                src="../images/ellucian_logo.png"
+                alt="Ellucian"
+                width={158}
+                height={48}
+              />
             </div>
           </div>
           <div
@@ -469,7 +492,8 @@ export default function Home() {
                     Questions we're often asked
                   </h2>
                   <p className="mt-4 text-base leading-7 text-gray-600">
-                    Can’t find the answer you’re looking for? Reach out to our{" "}
+                    Can’t find the answer you’re looking for? Schedule a demo,
+                    or reach out to our{" "}
                     <a
                       href="https://www.geckoengage.com/about/contact/"
                       target="_blank"
@@ -508,6 +532,7 @@ export default function Home() {
                     <a
                       href={item.href}
                       className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+                      target="_blank"
                     >
                       {item.name}
                     </a>
@@ -519,6 +544,7 @@ export default function Home() {
                   <a
                     key={item.name}
                     href={item.href}
+                    target="_blank"
                     className="text-gray-400 hover:text-gray-500"
                   >
                     <span className="sr-only">{item.name}</span>
@@ -537,10 +563,9 @@ export default function Home() {
           <div className=" absolute bottom-0 w-full mx-auto ">
             <div className="  w-11/12 mx-auto py-4 px-6 bg-slate-600 rounded-t-lg">
               <p className="text-white text-sm">
-                Note: this demo is designed to give you a flavour of what's
-                possible with AI. Gecko’s platform has advanced settings which
-                can constrain answers, focus on certain topics and give your AI
-                a distinct personality. Reach out for more information.
+                This automated demo was created using 200+ pages from the
+                ZZZ.ac.uk website. Schedule a demo to learn more about the
+                advanced settings within the Gecko platform.
               </p>
             </div>
           </div>
